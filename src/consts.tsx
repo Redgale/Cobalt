07f48f8edf8befa6aad080f1e8462ea1b6633e2d
Redgale <150551630+Redgale@users.noreply.github.com>
2026-04-18 14:08:39 -0400

    Update GitHub URL in consts.tsx

--- src/consts.tsx
+++ src/consts.tsx
@@ -1,7 +1,7 @@
 const obfuscation = true;
 // @ts-ignore
 const bareServerURL = new URL("/bare/", globalThis.location);
-const github = "https://github.com/cognetwork-dev/Cobalt";
+const github = "https://github.com/Redgale/Andesine";
 const discord = "https://discord.com/invite/yk33HZSZkU";
 
 export { obfuscation, bareServerURL, github, discord };
